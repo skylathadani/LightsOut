c8a072daeae0ff2012dafa8c2f78d7dfe0b14fb6
GregMitch <grmitch1@gmail.com>
2019-02-18 21:01:45 -0500

    Added Soultion.java file with constructors.

---
+++ Solution.java
@@ -0,0 +1,23 @@
+/**
+* @author: Gregory Mitchell
+* Student number: 300053307
+* Course: ITI 1121-A00
+* Assignment: 1
+
+* @author: Skyla Thadani
+* Student number: 300055455
+* Course: ITI 1121-C00
+* Assignment: 1
+*/
+
+public class Solution {
+	Boolean[][] x;
+	public Solution(int width, int height) {
+		x = new Boolean[width][height];
+	}
+
+	public Solution(Solution other) {
+		Solution n = new Solution;
+		n = other;
+	}	
+}
