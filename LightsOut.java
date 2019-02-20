3c0451b28e1133926c50fb52fe2707c9a8351d8f
Skyla Thadani <skylathadani@MacBook-Pro.local>
2019-02-20 10:20:28 -0500

    Finished 1.3

--- LightsOut.java
+++ LightsOut.java
@@ -1,15 +1,19 @@
 public class LightsOut{
 	public ArrayList<Solution> solve(int width, int height){
+		final long startTime = System.nanoTime();
 		partialSolutions = new ArrayListSolutionQueue();
 		Solution x = new Solution(width,height);
 		partialSolutions.enqueue(x);
 		ArrayList <Solutions> solutions = new ArrayList <Solutions> ();
 
-		while(!(partialSolutions.isEmpty()){
+		while(partialSolutions.isEmpty()==false){
 			Solution current = partialSolutions.dequeue();
 
 			if(current.isReady() && current.isSucessful()){
 				solutions.add(current);
+				final long duration = System.nanoTime() - startTime;
+
+				System.out.println("A solution has been found in " + duration);
 			}else if (current.isReady() == false){
 
 				Solution temp = new Solution(current);
@@ -28,6 +32,16 @@ public class LightsOut{
 
 	public static void main(String[] args) {
 
-		System.out.println("The number of solutions found is ")
+		solver = new solve(3,3);
+
+
+
+		System.out.println("The number of solutions found is " + solver.size());
+
+		System.out.println("The Solutions are:");
+
+		for(int i = 0, i < solver.size(); i++){
+			System.out.println(solver.get(i));
+		}
 	}
 }
